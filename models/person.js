4fc2939341bc29dd19ec5ceadfa16e9629bce11f
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-04-20 18:25:14 -0400

    Update person.js

--- models/person.js
+++ models/person.js
@@ -21,7 +21,7 @@ mongoose
 
 const personSchema = new mongoose.Schema({
 	name: { type: String, required: true, unique: true, minlength: 3 },
-	number: { type: String, required: true, minlength: 8 },
+	number: { type: String, required: true, minlength: 3 },
 });
 
 personSchema.plugin(uniqueValidator);
